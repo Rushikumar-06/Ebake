a8c8d727be4ce47b50d8a2f8ce1710ad5bc10fca
Rushikumar-06 <chundururushikumar@gmail.com>
2025-10-19 23:18:58 +0530

    update cookies for production

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -17,7 +17,7 @@ const generateToken = (userId, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
   });
 
